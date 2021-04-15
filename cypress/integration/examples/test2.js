0b9d4acebb6bbd408fdb64767b63d17a7edd6f7a
rodolforoc <rodolforoc97@gmail.com>
2021-04-15 15:17:25 -0300

    First commit

---
+++ cypress/integration/examples/test2.js
@@ -0,0 +1,22 @@
+/// <reference types="Cypress" />
+ 
+describe('My Second Test Suite', () => {
+  
+  it('My Second Test case',() => {
+    
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+    cy.get('.search-keyword').type('ca')
+    cy.wait(2000)
+    
+    cy.get('.products').as('productLocator')
+    cy.get('@productLocator').find('.product').each(($el, index, $list) => {
+    
+      const textVeg=$el.find('h4.product-name').text()
+      if(textVeg.includes('Cashews')) {
+        $el.find('button').click()
+      }
+    })
+
+    cy.get('.cart-icon > img').click()
+  })  
+})
