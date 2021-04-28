a416089efaaa89d4f12ef37397d992644e25b9ba
rodolforoc <rodolforoc97@gmail.com>
2021-04-28 15:41:48 -0300

    add cucumber to the project

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -8,7 +8,7 @@
 // You can read more here:
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
-
+const cucumber = require('cypress-cucumber-preprocessor').default
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
@@ -17,6 +17,5 @@
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  on('file:preprocessor', cucumber())
 }
